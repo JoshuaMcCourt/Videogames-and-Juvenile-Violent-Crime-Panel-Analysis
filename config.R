b4597943666fc4b5c73add0b13c2758d00c77db5
JoshuaMcCourt <joshuamccourt@hotmail.co.uk>
2026-03-04 11:58:30 +0000

    Update config.R

--- config.R
+++ config.R
@@ -1,5 +1,5 @@
 config = list(
-  ROOT = normalizePath("C:/Users/JoshuaMcCourt/Documents/Masters Work/Dissertation", winslash = "/"),
+  ROOT = normalizePath("C:/Users/JoshuaMcCourt/Documents/Masters Work/Dissertation/Videogame Juvenile Violence", winslash = "/"),
   IPUMS_RAW = "data/raw/ipums/CPSFull.csv",
   UCR_DIR = "data/raw/ucr/ucr_arrests_monthly_all_crimes_race_sex_1974_2020_dta",
   PROCESSED = "data/processed",
